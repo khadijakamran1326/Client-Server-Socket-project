13725c3441dbc0fe3ea906c385fdeb8c73b03234
khadijakamran1326 <khadijakamran746@gmail.com>
2025-10-16 11:46:34 +0500

    Add files via upload

---
+++ Client Socket.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Net.Sockets;
+using System.Text;
+class Client
+{
+    static void Main()
+    {
+        var client = new TcpClient("10.10.20.4", 5000);
+        var ns = client.GetStream();
+        Console.Write("Enter number:");
+        string num = Console.ReadLine();
+        ns.Write(Encoding.UTF8.GetBytes(num));
+        byte[] data = new byte[4096];
+        int bytes = ns.Read(data, 0, data.Length);
+        Console.WriteLine("\n"+
+            Encoding.UTF8.GetString(data, 0, bytes));
+        ns.Close();
+        client.Close();
+    }
+}
